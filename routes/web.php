572654246df568594c22baeed4ff5627b25de6f8
Adriano Miranda <adrjmiranda@gmail.com>
2025-11-16 21:36:43 -0300

    chore: created terms of use page

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,10 @@ use MovieStar\Application\Controllers\Pages\HomeController;
 use MovieStar\Application\Controllers\Pages\LoginController;
 use MovieStar\Application\Controllers\Pages\PrivacyPolicies;
 use MovieStar\Application\Controllers\Pages\RegisterController;
+use MovieStar\Application\Controllers\Pages\TermsOfUse;
 
 $app->get('/', [HomeController::class, 'index']);
 $app->get('/login', [LoginController::class, 'index']);
 $app->get('/registrar', [RegisterController::class, 'index']);
 $app->get('/politicas-de-privacidade', [PrivacyPolicies::class, 'index']);
-$app->get('/termos-de-uso', [PrivacyPolicies::class, 'index']);
+$app->get('/termos-de-uso', [TermsOfUse::class, 'index']);
